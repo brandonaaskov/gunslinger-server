20839ea8b72fdcb1e8e331621fddbc4d0de53900
Brandon Aaskov <baaskov@fullscreen.net>
2014-03-12 10:44:14 -0700

    Getting ready for deploy.

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,36 @@
 var _ = require('lodash'),
   gulp = require('gulp'),
   coffee = require('gulp-coffee'),
-  nodemon = require('gulp-nodemon'),
-  through = require('through');
+  nodemon = require('gulp-nodemon');
 
 var paths = {
   scripts: ['app.coffee', 'scripts/**/*.coffee']
 };
 
-gulp.task('scripts', function() {
-  // place code for your default task here
+//compiles coffee script files
+gulp.task('scripts', function () {
   gulp.src(paths.scripts)
     .pipe(coffee())
     .pipe(gulp.dest('./scripts'));
 });
 
-// Rerun the task when a file changes
-gulp.task('watch', function() {
+// recompile coffeescript files on change
+gulp.task('watch', function () {
   gulp.watch(paths.scripts, ['scripts']);
 });
 
 gulp.task('nodemon', function () {
-  var ignore = ['.git', 'node_modules', 'README.md'];
+  var ignoredFiles = ['.git', 'node_modules', 'README.md'];
   var options = {
     script: 'app.coffee',
-    ignore: ignore,
+    ignore: ignoredFiles,
     ext: 'js coffee json'
   };
 
-  nodemon(options)
-    .on('change', ['compile'])
-    .on('restart', function () {
-      console.log('restarted!')
-    });
+  nodemon(options).on('change', ['scripts']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['scripts', 'watch']);
+gulp.task('default', ['scripts', 'watch', 'nodemon']);
 gulp.task('compile', ['scripts']);
-gulp.task('develop', ['nodemon'])
+gulp.task('develop', ['nodemon']);
